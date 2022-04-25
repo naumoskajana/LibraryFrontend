de11ee5f08abe78e7f1c12cef1d5a13445ad8489
naumoskajana <jnaumoskaa@gmail.com>
2022-04-25 14:17:58 +0200

    api url change

--- src/custom-axios/axios.js
+++ src/custom-axios/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'http://localhost:8080/api',
+    baseURL: 'https://emtlab2backend193018.herokuapp.com',
     headers: {
         'Access-Control-Allow-Origin': '*'
     }
